74d1e5363ae9bb2a9566850d335a563703aeaa05
dacian <dacian@kuwaitnet.com>
2023-11-14 21:38:11 +0200

    change validation.yml

--- setup.py
+++ setup.py
@@ -20,14 +20,12 @@ test_requirements = [
 setup(
     author="Dacian Popute",
     author_email="dacian@ottu.com",
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
