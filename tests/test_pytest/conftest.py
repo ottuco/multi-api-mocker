fbb655c47cfee1c589a2311b9d87570fa0766e9b
Dacian Popute <dacian@ottu.com>
2025-06-27 00:40:27 +0300

    Fix: Resolve flake8 F401 unused import in conftest.py

--- tests/test_pytest/conftest.py
+++ tests/test_pytest/conftest.py
@@ -9,10 +9,8 @@ except ImportError:
     pass
 
 try:
-    from multi_api_mocker.contrib.pytest_plugin import (
-        setup_aiohttp_mocks,
-        aiohttp_available,
-    )  # noqa: F401
+    from multi_api_mocker.contrib.pytest_plugin import setup_aiohttp_mocks  # noqa: F401
+    from multi_api_mocker.contrib.pytest_plugin import aiohttp_available
 except ImportError:
     aiohttp_available = False
 
